078fcc7c1dda76c47e07f741852a1a9fcd00bc2b
Dario Villanueva <dario.villanueva@gmail.com>
2013-10-10 17:22:25 +0100

    printing colors

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-
+var cronJob = require('cron').CronJob;
 var scraper = require('./lib/dataScraper');
 
+
+// var job = new cronJob({
+//   // every minute, 
+//   cronTime: '0 * 8-17 * * 1-5',
+//   onTick: function() {
+//     // Runs every weekday, from 8 til 
+//     scraper.scrape();
+//   },
+//   start: true
+// });
+
 scraper.scrape();
+
